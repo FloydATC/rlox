15eca9e02d818a5ea9185485867a79e2eb6edec4
Andreas Lund <floyd@atc.no>
2021-03-29 16:31:51 +0200

    Added ParserRules for basic arithmetics

--- src/lox/parser.rs
+++ src/lox/parser.rs
@@ -60,12 +60,39 @@ impl Parser {
 impl Parser {
     fn binary(&mut self, _can_assign: bool) {
     }
+    fn unary(&mut self, _can_assign: bool) {
+    }
     fn rule(&self, kind: TokenKind) -> ParserRule {
         match kind {
-            TokenKind::PLUS => return ParserRule {
-                prefix: None,  infix: Some(Parser::binary),  precedence: ParserPrec::Term
+            TokenKind::MINUS => return ParserRule {
+                prefix: 	Some(Parser::unary), 
+                infix: 		Some(Parser::binary), 
+                precedence: 	ParserPrec::Term
             },
-            _ => panic!("No ParserRule defined for TokenKind {:?}", kind)
+            TokenKind::PLUS => return ParserRule {
+                prefix: 	None, 
+                infix: 		Some(Parser::binary), 
+                precedence: 	ParserPrec::Term
+            },
+            TokenKind::SLASH => return ParserRule {
+                prefix: 	None, 
+                infix: 		Some(Parser::binary), 
+                precedence: 	ParserPrec::Factor
+            },
+            TokenKind::STAR => return ParserRule {
+                prefix: 	None, 
+                infix: 		Some(Parser::binary), 
+                precedence: 	ParserPrec::Factor
+            },
+            // Default handler
+            _ => {
+                eprintln!("Warning: No ParserRule for TokenKind {:?}", kind);
+                return ParserRule {
+                    prefix: 	None, 
+                    infix: 		None, 
+                    precedence: 	ParserPrec::None
+                }
+            },
         }
     }
 }
