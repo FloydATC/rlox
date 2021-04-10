3dda08f60ffcacc6affdc9ed39082d76b33f1b5d
Andreas Lund <floyd@atc.no>
2021-04-10 11:38:49 +0200

    Cleaned out string related code that was moved to value earlier

--- src/lox/obj.rs
+++ src/lox/obj.rs
@@ -7,7 +7,6 @@ use super::closure::Closure;
 #[allow(dead_code)]
 #[derive(Debug)]
 pub enum Obj {
-    String(String),
     Function(Function),
     Closure(Closure),
 }
@@ -15,21 +14,11 @@ pub enum Obj {
 
 #[allow(dead_code)]
 impl Obj {
-    pub fn string(s: &str) -> Obj {
-        Obj::String(s.to_string())
-    }
     pub fn function(f: Function) -> Obj {
         Obj::Function(f)
     }
     pub fn closure(c: Closure) -> Obj {
         Obj::Closure(c)
-    }
-
-    pub fn is_string(&self) -> bool {
-        match self {
-            Obj::String(_) 	=> true,
-            _			=> false,
-        }
     }
 
     pub fn is_function(&self) -> bool {
@@ -48,15 +37,7 @@ impl Obj {
 
     pub fn is_truthy(&self) -> bool {
         match self {
-            Obj::String(s)	=> s != "",	// true if not empty
-            _			=> true,	// All other objects are truthy
-        }
-    }
-
-    pub fn as_string(&self) -> &String {
-        match self {
-            Obj::String(s) => return &s,
-            _ => panic!("{:?} is not a Function Object", self),
+            _			=> true,	// All objects are truthy (for now)
         }
     }
 
@@ -85,8 +66,7 @@ impl Obj {
 impl PartialEq for Obj {
     fn eq(&self, other: &Obj) -> bool { 
         match (self, other) {
-            (Obj::String(a), Obj::String(b))     => a == b,
-            // Obj types other than Obj::String must be same object
+            // Obj types must be same object
             (Obj::Function(a), Obj::Function(b)) => std::ptr::eq(a, b),
             (Obj::Closure(a), Obj::Closure(b))   => std::ptr::eq(a, b),
             _ => false, // Obj types mismatch
@@ -98,8 +78,7 @@ impl PartialEq for Obj {
 impl std::cmp::PartialOrd for Obj {
     fn partial_cmp(&self, other: &Obj) -> Option<std::cmp::Ordering> {
         match (self, other) {
-            (Obj::String(a), Obj::String(b)) => Some(a.cmp(b)),
-            _ => None, // Value types mismatch
+            _ => None, // Value types mismatch or can't be ordered
         }
     }
 }
@@ -108,10 +87,6 @@ impl std::cmp::PartialOrd for Obj {
 impl std::fmt::Display for Obj {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Obj::String(st) => {
-                // Escape non-ascii and non-printable ascii chars
-                write!(f, "Obj::String({:?})", st)
-            }
             Obj::Function(fu) => {
                 write!(f, "Obj::Function({})", fu.name())
             }
